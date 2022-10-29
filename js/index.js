00719ed55e6309cac4ab0898d9ff9c9c1c561d80
Abraham Jude <abrahamjude1999@gmail.com>
2022-10-29 04:40:21 -0700

    fourth commit

--- js/index.js
+++ js/index.js
@@ -1,12 +1,25 @@
 let currentYear = $("#year")
-contactUs = $("#contact")
-contactModal = $("#contactModal")
+inputs = $(".inputs")
+btnSend = $("#btnSend")
+span = $("#span")
+// console.log(inputs);
 
-contactUs.on("click", ()=>{
-    contactModal.show()
-})
+
+
 
 let Cyear = new Date().getFullYear();
 console.log(Cyear);
 
 currentYear.text(new Date().getFullYear());
+
+function validation(){
+    for(let i = 0; i < inputs.length; i++){
+        if(inputs[i].val == "" || inputs[i].val == null){
+            span.innerHTML = "Feild Required";
+        }
+    }
+}
+
+btnSend.on("click", function(){
+    validation()
+})
